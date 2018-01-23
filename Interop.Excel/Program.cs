e3d88d6166b143b582dfa90e08f351ebcfada02c
Peter Mei <peter.mei@showmgr.com>
2018-01-23 14:37:09 -0800

    Add project files.

---
+++ Interop.Excel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interop.Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
